23bc3be504587018a37be90d4fb437cfe523b432
TheDarkestNightRises <91905169+TheDarkestNightRises@users.noreply.github.com>
2022-05-01 17:36:43 +0200

    Deleted useless classes

--- src/musicApp/client/views/profile/src/Lauch.java
+++
@@ -1,9 +0,0 @@
-package musicApp.client.views.profile.src;
-
-import javafx.application.Application;
-
-public class Lauch {
-    public static void main(String[] args) {
-        Application.launch(Main.class);
-    }
-}
