54ad6da8e3f767a51399ed04b94eedde0be00fab
TheDarkestNightRises <91905169+TheDarkestNightRises@users.noreply.github.com>
2022-05-03 13:17:54 +0200

    Added login design

--- src/musicApp/client/views/loadingScreen/LoadingScreenViewController.java
+++ src/musicApp/client/views/loadingScreen/LoadingScreenViewController.java
@@ -1,8 +1,10 @@
 package musicApp.client.views.loadingScreen;
 
+import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
 public class LoadingScreenViewController {
+    @FXML
     public Text progress;
 
     public void setTextProgress(String text) {
