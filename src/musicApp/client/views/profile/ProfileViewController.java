e8a533168b28688630ff5edadd08da630690dd8c
TheDarkestNightRises <91905169+TheDarkestNightRises@users.noreply.github.com>
2022-05-14 00:48:38 +0200

    Update ProfileViewController.java

--- src/musicApp/client/views/profile/ProfileViewController.java
+++ src/musicApp/client/views/profile/ProfileViewController.java
@@ -38,6 +38,10 @@ public class ProfileViewController implements ViewController {
         }
         System.out.println(playlists);
         if (playlists == null) return;
+        initPlaylists(playlists);
+    }
+
+    public void initPlaylists(ArrayList<Playlist> playlists){
         for (Playlist playlist : playlists) {
             Text playlistTitleText = vh.generateLoadingText();
             profileContainer.getChildren().add(playlistTitleText);
@@ -52,152 +56,5 @@ public class ProfileViewController implements ViewController {
                 });
             }).start();
         }
-
-        //    @Override
-//    public void init(ViewHandler vh, ViewModelFactory vmf) {
-//        this.vh = vh;
-//        this.viewModel = vmf.getProfileViewModel();
-//        // ----------------------------------SEGREGATE PLZ-----------------------------------
-//        recentlyPlayed = new ArrayList<>(getRecentlyPlayed());
-//        favorites = new ArrayList<>(getFavorites());
-//
-//        try {
-//            for (Song song : recentlyPlayed) {
-//                FXMLLoader fxmlLoader = new FXMLLoader();
-//                fxmlLoader.setLocation(getClass().getResource("song.fxml"));
-//
-//                VBox vBox = fxmlLoader.load();
-//                SongController songController = fxmlLoader.getController();
-//                songController.setData(song);
-//
-//                recentlyPlayedContainer.getChildren().add(vBox);
-//            }
-//
-//            for (Song song : favorites) {
-//                FXMLLoader fxmlLoader = new FXMLLoader();
-//                fxmlLoader.setLocation(getClass().getResource("song.fxml"));
-//
-//                VBox vBox = fxmlLoader.load();
-//                SongController songController = fxmlLoader.getController();
-//                songController.setData(song);
-//
-//                favoriteContainer.getChildren().add(vBox);
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//
-//
-//
-//    // ----------------------------------DUMMY DATA-----------------------------------
-//    private List<Song> getRecentlyPlayed() {
-//        List<Song> ls = new ArrayList<>();
-//
-//        Song song = new Song();
-//        song.setTitle("In The Name Of Love");
-//
-//        song.setPicture_path("../img/In_the_Name_of_Love.jpeg");
-//        ls.add(song);
-//
-//        song = new Song();
-//        song.setTitle("Emri Olur");
-//
-//        song.setPicture_path("../img/emri_olur.jpg");
-//        ls.add(song);
-//
-//        song = new Song();
-//        song.setTitle("You are my sunshine");
-//
-//        song.setPicture_path("../img/jasmin_thompson.jpg");
-//        ls.add(song);
-//
-//        song = new Song();
-//        song.setTitle("On My Way");
-//
-//        song.setPicture_path("../img/on_my_way.jpg");
-//        ls.add(song);
-//
-//        song = new Song();
-//        song.setTitle("Let Me Love You");
-//
-//        song.setPicture_path("../img/let_me_love_you.jpg");
-//        ls.add(song);
-//
-//        song = new Song();
-//        song.setTitle("It Ain't Me");
-//
-//        song.setPicture_path("../img/It-Ain-t-Me.jpg");
-//        ls.add(song);
-//
-//        song = new Song();
-//        song.setTitle("One Dance");
-//
-//        song.setPicture_path("../img/one_dance.jpg");
-//        ls.add(song);
-//
-//        song = new Song();
-//        song.setTitle("Faded");
-//
-//        song.setPicture_path("../img/faded.jpg");
-//        ls.add(song);
-//
-//        return ls;
-//    }
-//
-//    public List<Song> getFavorites() {
-//        List<Song> ls = new ArrayList<>();
-//
-//        Song song = new Song();
-//        song.setTitle("Mühür");
-//
-//        song.setPicture_path("../img/mühür.jpg");
-//        ls.add(song);
-//
-//        song = new Song();
-//        song.setTitle("Faded");
-//
-//        song.setPicture_path("../img/faded.jpg");
-//        ls.add(song);
-//
-//        song = new Song();
-//        song.setTitle("Señorita");
-//
-//        song.setPicture_path("../img/seniorita.jpg");
-//        ls.add(song);
-//
-//        song = new Song();
-//        song.setTitle("Can't Help Falling in love");
-//
-//        song.setPicture_path("../img/cant_help_falling_in_love.jpg");
-//        ls.add(song);
-//
-//        song = new Song();
-//        song.setTitle("Friend");
-//
-//        song.setPicture_path("../img/friendjpg.jpg");
-//        ls.add(song);
-//
-//        song = new Song();
-//        song.setTitle("No Time To Die");
-//
-//        song.setPicture_path("../img/no_time_to_die.jpg");
-//        ls.add(song);
-//
-//        song = new Song();
-//        song.setTitle("Emri Olur");
-//
-//        song.setPicture_path("../img/emri_olur.jpg");
-//        ls.add(song);
-//
-//        song = new Song();
-//        song.setTitle("Wolves");
-//
-//        song.setPicture_path("../img/wolves.jpg");
-//        ls.add(song);
-//
-//        return ls;
-//    }
     }
 }
