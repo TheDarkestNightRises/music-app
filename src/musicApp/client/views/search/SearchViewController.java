7ed15706171936d942f8734fb94c89264faf4728
TheDarkestNightRises <91905169+TheDarkestNightRises@users.noreply.github.com>
2022-05-17 13:55:53 +0200

    Update SearchViewController.java

--- src/musicApp/client/views/search/SearchViewController.java
+++ src/musicApp/client/views/search/SearchViewController.java
@@ -4,8 +4,12 @@ package musicApp.client.views.search;
 import javafx.application.Platform;
 import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import musicApp.client.core.ViewController;
 import musicApp.client.core.ViewHandler;
 import musicApp.client.core.ViewModelFactory;
@@ -34,12 +38,23 @@ public class SearchViewController implements ViewController {
     }
 
     private void showSearchResults(PropertyChangeEvent event) {
+        Text loadingText = viewHandler.generateLoadingText();
+        HBox containerHBox = new HBox();
+        containerHBox.setAlignment(Pos.CENTER_LEFT);
+        containerHBox.setPadding(new Insets(10));
+        containerHBox.setSpacing(10);
+        containerHBox.getChildren().add(loadingText);
+        searchContainer.getChildren().add(containerHBox);
+        VBox vBoxContainer = new VBox();
+        searchContainer.getChildren().add(vBoxContainer);
         new Thread(()->{
             FilteredList<Song> filteredList = (FilteredList<Song>) event.getNewValue();
             ArrayList<Song> songs = new ArrayList<>(filteredList);
             Platform.runLater(()->{
                 searchContainer.getChildren().clear();
+                containerHBox.getChildren().add(viewHandler.generateTitleHBox(songs));
                 searchContainer.getChildren().add(viewHandler.generateView(songs));
+                loadingText.setText("SearchDone");
             });
         }).start();
     }
