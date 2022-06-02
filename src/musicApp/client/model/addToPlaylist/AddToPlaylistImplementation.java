b89ce29f8de60219e64d1bd54eb87d52e532211f
TheDarkestNightRises <91905169+TheDarkestNightRises@users.noreply.github.com>
2022-06-02 07:01:51 +0200

    Revert "deleted img"
    This reverts commit 32afcb4559ab78cb173c3d554eb536828a86580b.

--- src/musicApp/client/model/addToPlaylist/AddToPlaylistImplementation.java
+++ src/musicApp/client/model/addToPlaylist/AddToPlaylistImplementation.java
@@ -5,9 +5,7 @@ import musicApp.server.model.domainModel.Playlist;
 import musicApp.server.model.domainModel.Song;
 import musicApp.server.model.domainModel.User;
 
-/**
- * The interface for the AddAlbumManager,model in the client.It's used to add albums
- */
+
 public class AddToPlaylistImplementation implements AddToPlaylistManager
 {
   private Client client;
